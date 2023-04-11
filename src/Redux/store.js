cb01c92a712006cca5c12f6e9ca19b771374ab9b
Your Name <you@example.com>
2023-04-11 15:58:25 -0300

    se puede ya ?

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,1 +1,8 @@
-bla bla bla
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './Reducer';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export default store;
