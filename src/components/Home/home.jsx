a29f5df940a0577a78a95d0d2213ae3836ee75a3
Tobyvallejos99 <120630105+Tobyvallejos99@users.noreply.github.com>
2023-04-12 10:34:12 -0300

    Update home.jsx

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,12 +1,1 @@
-// import NavBar from './NavBar/navbar.jsx';
-// import Cards from './Cards/cards.jsx'
 
-
-export default function Home() {
-    return (
-        <div>
-        <NavBar/>
-        <Cards/>
-        </div>
-    )
-}
