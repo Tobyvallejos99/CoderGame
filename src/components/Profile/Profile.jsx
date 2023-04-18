6548d17800bf2b451c90b5fe19d3af535a23a925
ferromayy <ferromay.is@icloud.com>
2023-04-18 19:47:53 -0300

    profileCss

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import style from "./Profile.module.css";
+
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
@@ -9,7 +11,7 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
+      <div className={style.container}>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
