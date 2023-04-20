5ea338a38614f6f87d75ee387381f13f7efa6bc5
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-04-20 19:20:35 -0300

    paginado con puntitos suspensivos

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,21 +5,26 @@ import { setPage } from "../../Redux/actions/actions";
 
 export default function Pagination ({gamesPerPage, games}) {
     const pageNumbers = []
-const dispatch = useDispatch()
+    const dispatch = useDispatch()
+
     for (let i = 1; i <= Math.ceil(games/gamesPerPage); i++) {
         pageNumbers.push(i);
-        
     }
 
     return(
         <nav className={style.nav_container}>
             <ul className={style.ul_container}> 
                 {pageNumbers && 
-                pageNumbers.map(number => (
+                pageNumbers.slice(0, 20).map(number => (
                     <li className={style.li_container} onClick={() => dispatch(setPage(number))} key={number}>
                         <button className="btn btn-danger" type="button">{number}</button> 
                     </li>
                 ))}
+                {pageNumbers.length > 20 && (
+                    <li className={style.li_container}>
+                        <button className="btn btn-danger" type="button">...</button>
+                    </li>
+                )}
             </ul>
         </nav>
     ) 
