c1aa504fbc4032019988aadc13a6d3a346a72fc1
ferromayy <ferromay.is@icloud.com>
2023-04-24 10:05:52 -0300

    auth0autorizacion

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ ReactDOM.render(
         authorizationParams={{
           redirect_uri: window.location.origin,
         }}
+        scope="openid,name,email,roles"
       >
         <App />
       </Auth0Provider>
@@ -23,3 +24,30 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import store from "./Redux/store/store";
+// import { Provider } from "react-redux";
+// import { Auth0Provider } from "@auth0/auth0-react";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <Auth0Provider
+//         domain="dev-5j3qqtfths0frdeu.us.auth0.com"
+//         clientId="5gO8jG2KYeQZzcwSNYWbHVPqwayBvG3f"
+
+//         authorizationParams={{
+//           redirect_uri: window.location.origin,
+//         }}
+//       >
+//         <App />
+//       </Auth0Provider>
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
