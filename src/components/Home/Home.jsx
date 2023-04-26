aa2c9a0e27f148be981b43a0bd0d0ced0074abfb
Your Name <you@example.com>
2023-04-26 14:12:16 -0300

    Chat Bot

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,20 @@
 import CardsHome from "../Cards/cardsHome";
+import React from "react";
 import Slider from "../Slider/Slider";
 import NavBar from "../NavBar/NavBar";
 import style from "../Home/cards.module.css";
 import { useDispatch } from "react-redux";
 import { postUser } from "../../Redux/actions/actions";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import CartBtn from "../CartBtn/CartBtn";
+import ChatBot from "react-simple-chatbot";
+import { ThemeProvider } from 'styled-components';
 
 const Home = () => {
   const dispatch = useDispatch();
   const { user, isAuthenticated } = useAuth0();
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -19,6 +23,90 @@ const Home = () => {
       console.log(userInfo);
     }
   }, [isAuthenticated, user, dispatch]);
+
+  const theme = {
+    background: ' rgba(130, 130, 130, 0.37)',
+    fontFamily: 'Helvetica Neue',
+    headerBgColor: 'rgba(117, 2, 2, 0.781)',
+    headerFontColor: '#fff',
+    headerFontSize: '15px',
+    botBubbleColor: 'rgba(117, 2, 2, 0.781)',
+    botFontColor: '#fff',
+    userBubbleColor: '#4a4a4a',
+    userFontColor: '#fff',
+  };
+  
+  const steps = [
+
+    {
+      id: "Greet",
+      message: "Hello, Welcome to CoderGame",
+      trigger: "Done",
+    },
+
+    {
+      id: "Done",
+      message: "Please enter your name!",
+      trigger: "waiting1",
+    },
+
+    {
+      id: "waiting1",
+      user: true,
+      trigger: "Name",
+    },
+
+    {
+      id: "Name",
+      message: "Hello, {previousValue}, how can we help you",
+      trigger: "issues",
+    },
+
+    {
+      id: "issues",
+      options: [
+        {
+          value: "Buy",
+          label: "Buy",
+          trigger: "Buy",
+        },
+        { value: "Sell", 
+          label: "Sell",
+          trigger: "Sell",
+        },
+        {
+          value: "Other",
+          label: "Other",
+          trigger: "Other",
+        },  
+      ],
+    },
+
+    {
+      id: "Buy",
+      message:
+        "To be able to buy you must first log in, then in the profile tab load balance and from the shopping card you buy your favorite games!",
+      end: true,
+    },
+
+    {
+      id: "Sell",
+      message:
+        "In order to sell you must have the COMPANY verification and the sale of products will be enabled.",
+      end: true,
+    },
+    {
+      id: "Other",
+      message: "Thank you for leaving your comment, our team will solve the problem as soon as possible and we will add more options to the list :D",
+      end: true,
+      },
+  ]; 
+
+  const handleToggleChatbot = () => {
+    setIsChatbotOpen(!isChatbotOpen);
+  }
+
+
   return (
     <div>
       <NavBar />
@@ -42,6 +130,22 @@ const Home = () => {
         <p />
         <CardsHome />
       </div>
+      <div
+  className={style.chatbot}
+  style={{ width: isChatbotOpen ? "400px" : "40px" }}
+>
+  {isChatbotOpen ? (
+    <div className={style.chatbot}>
+      
+      <ThemeProvider theme={theme}>
+        
+        <ChatBot steps={steps} />;
+      </ThemeProvider><button className="btn btn-danger" onClick={handleToggleChatbot}>X</button>
+    </div>
+  ) : (
+    <button className="btn btn-danger" onClick={handleToggleChatbot}>Open Chat:D</button>
+  )}
+</div>
     </div>
   );
 };
