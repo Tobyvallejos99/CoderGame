deee39cdf255ad7321141df65036989cdd36b919
Cesar <cleivaj93@gmail.com>
2023-04-27 15:59:04 -0400

    directorio de tremor

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,8 +1,16 @@
+import { Title } from '@tremor/react';
 import style from './DashBoard.module.css';
+import SellerDashBoard from './SellerDashBoard';
+import '@tremor/react'
 
 const DashBoard = () => {
     return(
-        <div className={style.container}></div>
+        <div className={style.container}>
+            <Title className={style.title}>Dashboard</Title>
+            {/* aqui va la card profile
+            aqui traer una dhasboard segun tu userRol */}
+            <SellerDashBoard />
+        </div>
     )
 }
 
---
+++ src/components/DashBoard/SellerDashBoard.jsx
@@ -0,0 +1,13 @@
+import { Card } from "@tremor/react";
+import TableGamesToSell from "./DashBoardComponents/TableGamesToSell/TableGamesToSell";
+
+
+const SellerDashBoard = () => {
+    return(
+        <Card>
+            <TableGamesToSell />
+        </Card>
+    )
+}
+
+export default SellerDashBoard;
