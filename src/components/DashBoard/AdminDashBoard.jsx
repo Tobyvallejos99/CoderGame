8bf9bcac5a902db0ba93ef2d2da276b1bc4651bb
Cesar <cleivaj93@gmail.com>
2023-04-28 13:27:10 -0400

    allgamescomments table for admins

--- src/components/DashBoard/AdminDashBoard.jsx
+++ src/components/DashBoard/AdminDashBoard.jsx
@@ -1,10 +1,12 @@
 import { Card, Text, Title } from "@tremor/react";
+import AllGamesComments from "./DashBoardComponents/AllGamesComments/AllGamesComments";
 import AllUsers from "./DashBoardComponents/AllUsers/AllUsers";
 
 export default () => {
     return(
         <Card>
-            <AllUsers/>
+            <AllUsers />
+            <AllGamesComments />
         </Card>
     )
 }
---
+++ src/components/DashBoard/DashBoardComponents/AllGamesComments/AllGamesComments.jsx
@@ -0,0 +1,10 @@
+import { Card } from "@tremor/react";
+import style from './AllGamesComments.module.css';
+
+export default () => {
+    return(
+        <Card className={style.container}>
+            hola
+        </Card>
+    )
+}
