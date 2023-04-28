be5f73d2cb66891a0218a51f7ce10ca917e90d0d
ferromayy <ferromay.is@icloud.com>
2023-04-28 08:42:02 -0300

    TableUserDashboard

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,18 +1,19 @@
-import { Title } from '@tremor/react';
-import style from './DashBoard.module.css';
-import SellerDashBoard from './SellerDashBoard';
-import '@tremor/react'
+import { Title } from "@tremor/react";
+import style from "./DashBoard.module.css";
+import SellerDashBoard from "./SellerDashBoard";
+import UserDashBoard from "./UserDashBoard";
+import "@tremor/react";
 
 const DashBoard = () => {
-    return(
-        <div className={style.container}>
-            <Title className={style.title}>Dashboard</Title>
-            {/* aqui va la card profile
+  return (
+    <div className={style.container}>
+      <Title className={style.title}>Dashboard</Title>
+      {/* aqui va la card profile
             aqui traer una dhasboard segun tu userRol */}
-            <SellerDashBoard />
-        </div>
-    )
-}
+      <SellerDashBoard />
+      <UserDashBoard />
+    </div>
+  );
+};
 
 export default DashBoard;
-
