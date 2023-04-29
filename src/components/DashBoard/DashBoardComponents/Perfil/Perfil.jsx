191833f79ac67c2dc29752bd2414ecbcd6ea4cbb
Cesar <cleivaj93@gmail.com>
2023-04-29 15:49:15 -0400

    perfil fixing

--- src/components/DashBoard/DashBoardComponents/Perfil/Perfil.jsx
+++ src/components/DashBoard/DashBoardComponents/Perfil/Perfil.jsx
@@ -5,14 +5,15 @@ import { useState } from "react";
 import { useEffect } from "react";
 import style from './Perfil.module.css';
 import pencil from './edit.svg';
+import { Link } from "react-router-dom";
 
 export default () => {
     const {user} = useAuth0();
     const [userInfo, setUserInfo] = useState(null);
     const [isActive, setIsActive] = useState(false);
     const [input, setInput] = useState({
-        img: '',
-        yt: '',
+        image: '',
+        linkYoutube: '',
         description: ''
     });
 
@@ -28,35 +29,51 @@ export default () => {
         isActive ? setIsActive(false) : setIsActive(true)
     }
 
+    const submitHandle = async (e) => {
+        e.preventDefault();
+        const sub = user.sub;
+        const update = {
+            image: input.image.length > 0 ? input.image : userInfo.profile.image,
+            linkYoutube: input.linkYoutube.length > 0 ? input.linkYoutube : userInfo.profile.linkYoutube,
+            description: input.description.length > 0 ? input.description : userInfo.profile.description
+        }
+        console.log(update, sub)
+        await axios.put(`http://localhost:3001/user/profile`, {sub, update});
+        setInput({
+            image: '',
+            linkYoutube: '',
+            description: ''
+        });
+    }
+
     useEffect(() =>{
         const loadData = async () =>{
             const {data} = await axios(`http://localhost:3001/user/profile/bybalance/${user.sub}`);
             setUserInfo(data);
         }
         loadData();
-    },[])
-    console.log(userInfo)
+    },[]);
     
     return(
         <Card className={style.container}>
             <img src={userInfo?.profile.image || user?.picture} alt="F" />
             <button onClick={editHandler}><img className={style.pencil} src={pencil} alt="" /></button>
-            {isActive && <div>
+            
+            {isActive && <form onSubmit={submitHandle}>
                             <label> Img:</label>
-                            <TextInput name="img" placeholder="insert img link here!!" />
+                            <TextInput name="image" value={input.image} onChange={handleChange} placeholder="insert img link here!!" />
                             <label> Youtube Link:</label>
-                            <TextInput name="yt" placeholder="Text Here!!" />
+                            <TextInput name="linkYoutube" value={input.linkYoutube} onChange={handleChange} placeholder="Text Here!!" />
                             <label> Descripcion:</label>
-                            <TextInput name="description" placeholder="Text Here!!" />
+                            <TextInput name="description" value={input.description} onChange={handleChange} placeholder="Text Here!!" />
                             <button>Confirm</button>
-                        </div>}
+                        </form>}
             <h2>{user?.given_name} - ID: ({userInfo?.profile.id})</h2>
             <h4>{user?.email}</h4>
             <p>Coins Balance:</p>
             <Metric>{userInfo?.balance.balance} coins.</Metric>
-            <p>Youtube link: </p>
-            
-            
+            <Link className="btn btn-outline-danger" to={"/payment"}>Buy Coins</Link>
+            <p>Youtube link: </p>            
             <Text>{userInfo?.profile.linkYoutube}</Text>
             <p>Description: </p>
             <Text>{userInfo?.profile.description}</Text>
