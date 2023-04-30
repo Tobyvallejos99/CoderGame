c85df173f408ea75504cac4312e1fb43fafb689f
Cesar <cleivaj93@gmail.com>
2023-04-30 12:13:21 -0400

    perfil fuera de las dashboard

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -5,14 +5,17 @@ import UserDashBoard from "./UserDashBoard";
 import "@tremor/react";
 import AdminDashBoard from "./AdminDashBoard";
 import NavBar from '../NavBar/NavBar'
+import Perfil from "./DashBoardComponents/Perfil/Perfil";
 
 const DashBoard = () => {
   return (
     <div className={style.container}>
       <NavBar />
       <Title className={style.title}>Dashboard</Title>
+
       {/* aqui va la card profile
             aqui traer una dhasboard segun tu userRol */}
+      <Perfil />
       {/* <SellerDashBoard /> */}
       {/* <AdminDashBoard /> */}
       <UserDashBoard />
