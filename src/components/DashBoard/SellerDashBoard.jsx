91a125d94609f34d9dac241b39f1fe9f7ec4634f
ferromayy <ferromay.is@icloud.com>
2023-05-02 08:56:51 -0300

    sellerDashboardProcess

--- src/components/DashBoard/SellerDashBoard.jsx
+++ src/components/DashBoard/SellerDashBoard.jsx
@@ -2,12 +2,14 @@ import { Card } from "@tremor/react";
 import GamesComment from "./DashBoardComponents/GamesComment/GamesComment";
 import TableGamesToSell from "./DashBoardComponents/TableGamesToSell/TableGamesToSell";
 import SellerBalance from "./SellerBalance/SellerBalance";
+import DonutSeller from "./DonutSeller/DonutSeller";
 const SellerDashBoard = () => {
   return (
     <div className="text-center">
       <TableGamesToSell />
       <GamesComment />
       <SellerBalance />
+      <DonutSeller />
     </div>
   );
 };
