9f8dc3905f0d16a395684152dc31aad477a329ea
ferromayy <ferromay.is@icloud.com>
2023-05-01 22:01:08 -0300

    SellerDashboardsChange

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -4,7 +4,7 @@ import SellerDashBoard from "./SellerDashBoard";
 import UserDashBoard from "./UserDashBoard";
 import "@tremor/react";
 import AdminDashBoard from "./AdminDashBoard";
-import NavBar from '../NavBar/NavBar'
+import NavBar from "../NavBar/NavBar";
 import Perfil from "./DashBoardComponents/Perfil/Perfil";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
@@ -13,41 +13,41 @@ import axios from "axios";
 
 const DashBoard = () => {
   const { user } = useAuth0();
-  const [ userInfo, setUserInfo] = useState(null)
-  
-  useEffect(() =>{
-    const loadData = async () =>{
-        const {data} = await axios(`http://localhost:3001/user/rol/${user.sub}`);
-        setUserInfo(data);
-    }
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const { data } = await axios(
+        `http://localhost:3001/user/rol/${user.sub}`
+      );
+      setUserInfo(data);
+    };
     loadData();
-},[]);
+  }, []);
 
   return (
-    <div >
+    <div>
       <NavBar />
-    <div className={style.container}>
-      
-      <Title className={style.title}>Dashboard</Title>
-      
+      <div className={style.container}>
+        <Title className={style.title}>Dashboard</Title>
+
         {/* aqui va la card profile
               aqui traer una dhasboard segun tu userRol */}
-          
-          {/* {userInfo.rol === 'admin' ? <AdminDashBoard /> : userInfo.rol === 'seller' ? <SellerDashBoard /> : <UserDashBoard />} */}
-          <div className={style.cardContainer}>
-            <Perfil/>
-            <AdminDashBoard/>
-          </div>
-          {/* <div className={style.cardContainer}>
-            <Perfil/>
-            <SellerDashBoard />
-          </div> */}
-          {/* <div className={style.cardContainer}>
+
+        {/* {userInfo.rol === 'admin' ? <AdminDashBoard /> : userInfo.rol === 'seller' ? <SellerDashBoard /> : <UserDashBoard />} */}
+        {/* <div className={style.cardContainer}>
+          <Perfil />
+          <AdminDashBoard />
+        </div> */}
+        <div className={style.cardContainer}>
+          <Perfil />
+          <SellerDashBoard />
+        </div>
+        {/* <div className={style.cardContainer}>
             <Perfil/>
             <UserDashBoard/>
           </div> */}
-
-    </div>
+      </div>
     </div>
   );
 };
