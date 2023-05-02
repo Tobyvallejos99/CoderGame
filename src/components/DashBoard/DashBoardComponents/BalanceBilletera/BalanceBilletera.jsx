5da5fc461970ba20fc9b205990bd027fd48639d8
mario19martinez <mariomartinez678abc@gmail.com>
2023-05-02 18:03:22 -0500

    Balance Funcional

--- src/components/DashBoard/DashBoardComponents/BalanceBilletera/BalanceBilletera.jsx
+++ src/components/DashBoard/DashBoardComponents/BalanceBilletera/BalanceBilletera.jsx
@@ -4,48 +4,32 @@ import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import axios from "axios";
-// const chartdata = [
-//     {
-//       name: "Balance de Juegos Comprados",
-//       "Balance": 34734,
-//       delta: '9.5%',
-//     },
-//     {
-//       name: "Saldo de Coins",
-//       "Balance":1445,
-//       delta: '5.1%',
-//     },
-//   ]
-
-//   const dataFormatter = (number: number)  => 
-//     `$ ${Intl.NumberFormat("us").format(number).toString()}`;
 
 export default function Balance ()  {
-    const [userInfo, setUserInfo] = useState({sallers: []});
+    const [userInfo, setUserInfo] = useState('');
     const  { user } = useAuth0();
 
     useEffect(() => {
         const sub = user?.sub
-        //const loadData = async () => {
-            //console.log(loadData)
-            const {data} = axios.get(`http://localhost:3001/admin/allusers?sub=${sub}`);
+        const loadData = async () =>{
+            const {data} = await axios.get('https://localhost:3001/admin/balance/' + sub);
             setUserInfo(data);
-        
-         //loadData();
-        //console.log(loadData)
-        
+            
+        }
+        loadData()
     }, []);
 
-    const balance = userInfo?.sellers ? userInfo.sallers.reduce((total, seller) => total + seller.Balance, 0) : 0;
+    const balance = userInfo?.clients ? userInfo?.clients.reduce((total, sellers) => total + sellers.totalBalance, 0) : 0;
 
     return(
         <>
+        
         <Card>
             <Flex>
                 <Text>Total Balance</Text>
-                <BadgeDelta text={balance} />
+                {balance}
             </Flex>
-            <Metric>${balance}</Metric>
+            <Metric></Metric>
         </Card>
         
         </>
